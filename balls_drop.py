bd6719534bd617cc713e9eb3b4440ae77a53da10
duruwell <saomaster4@gmail.com>
2025-09-30 22:58:46 +0900

    소년 설정 , 잔디 수정

--- balls_drop.py
+++ balls_drop.py
@@ -1,21 +1,54 @@
-from pico2d import *
 import random
-
-class Boy:
-    def __init__(self):
-    self.x, self.y = random.randint(100, 700), 90
-    slef.frame = 0
-    self.image = load_image('run_animation.png')
-
-
-class Ball:
-    def __init__(self):
-        self.imgae = load image('ball21x21.png')
-#  공 유닛
+from pico2d import *
 
 class   grass:
     def __init__(self):
         self.image = load_image('grass.png')
-    def.draw(self)
+
+    def draw(self):
         self.image.draw(400, 30)
-    def update(self): pass
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__ (self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+class Ball:
+    def __init__(self):
+         self.image= load_image()('ball21x21.png')
+        self.x, self.y = random.randint(100, 700), 150
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
+
+
+
+
+open_canvas()
+
+
+
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+
+
+    delay(0.05)
+
+close_canvas()
